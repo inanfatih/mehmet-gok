51877b22af965cf7ac7fbad1d724924c98a4090e
Fatih <mfatihinan@gmail.com>
2020-07-31 02:49:21 -0400

    Page is made resposive to smartphone sizes

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,23 +8,25 @@ function About() {
     <div id='about'>
       <div className='container'>
         <div className='about'>
-          <div className='about-image' />
-          <div className='about-text'>
-            <div>
-              <div className='my-1 about-title'>ABOUT</div>
-              <p className="about-content">
-                Muhammet Gok is a Turkish director. He was born in
-                Kahramanmaras, Turkey. He has been living in Germany. He is
-                married. He has a daughter named Sara. Muhammet speaks Turkish,
-                Russian, and English.
-                <br />
-                <br />
-                Muhammet Gok directed many tv series and commercials in Turkey,
-                Germany, Tunisia, and Algeria. He started to prepare a new tv
-                series in Tunisia.
-              </p>
+          <div className='about-title'>ABOUT</div>
+          <div className='about-content'>
+            <div className='about-image' />
+            <div className='about-text'>
+              <div>
+                <p className='about-content'>
+                  Muhammet Gok is a Turkish director. He was born in
+                  Kahramanmaras, Turkey. He has been living in Germany. He is
+                  married. He has a daughter named Sara. Muhammet speaks
+                  Turkish, Russian, and English.
+                  <br />
+                  <br />
+                  Muhammet Gok directed many tv series and commercials in
+                  Turkey, Germany, Tunisia, and Algeria. He started to prepare a
+                  new tv series in Tunisia.
+                </p>
+              </div>
+              <p className='my-1 mg-text'>MG</p>
             </div>
-            <p className='my-1 mg-text'>MG</p>
           </div>
         </div>
       </div>
